76dd231837f7269943ff48c5d6fd2b6da43f5c26
Akhil7K <146330175+Akhil7K@users.noreply.github.com>
2024-04-02 18:33:52 +0530

    updated our services content

--- src/components/ourServices/index.tsx
+++ src/components/ourServices/index.tsx
@@ -40,7 +40,7 @@ const OurServices = ({ setSelectedPage }: Props) => {
           <div className="flex flex-col items-center justify-center text-center md:w-full">
             <HText><div className="text-6xl sm:text-4xl">What We Offer</div></HText> 
             <p className="px-2 mt-4 text-2xl leading-relaxed md:py-12">
-            Raindrops Tech Solutions is dedicated to providing innovative and cutting-edge solutions to our clients. We specialize in developing software, designing websites, and providing IT consulting services. Our team of experts is passionate about technology and is committed to delivering the best possible results for our clients. We believe in building long-term relationships with our clients and providing them with the highest level of customer service. Our goal is to help our clients achieve their business objectives by leveraging the latest technologies and best practices in the industry.
+            Raindrops Tech Solutions is DEDICATED to providing innovative and cutting-edge solutions to our clients.<br />We SPECIALIZE in developing software, designing websites, and providing IT consulting services.<br /> Our team of experts is passionate about technology and is committed to DELIVERING the best possible results for our clients.<br />We believe in building long-term RELATIONSHIPS with our clients and providing them with the highest level of customer service.<br />Our goal is to help our clients achieve their business objectives by leveraging the latest TECHNOLOGIES and best practices in the industry.
             </p>
           </div>
         </motion.div>
