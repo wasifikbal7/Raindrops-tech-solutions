b6819d5bc065350f198f20acf2cc67c463f2189c
Akhil7K <146330175+Akhil7K@users.noreply.github.com>
2024-04-13 15:56:17 +0530

    Slider changes-our services

--- src/components/ourServices/slide.tsx
+++ src/components/ourServices/slide.tsx
@@ -35,7 +35,7 @@ export function CarouselSize() {
                 {Services.map((item: ClassType, index) => (                                          
                     <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                         <div className="items-center p-1 ml-3 w-fit md:h-4/5 md:w-11/12 xs:mx-2">
-                            <Card className="bg-transparent">
+                            <Card className="bg-transparent border-none border-transparent">
                                 <CardContent className="flex items-center justify-center p-6 bg-transparent hover:cursor-pointer aspect-square">
                                     <div className="p-1">
                                         <ImageCard
