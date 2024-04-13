e781d7d30acca63263350d09c01efa1a1e3dce86
wasifikbal7 <152157697+wasifikbal7@users.noreply.github.com>
2024-04-13 22:01:28 +0530

    Update PdfDownloadButton component with changed button styling (padding) and text

--- src/components/ourServices/pdf_download_btn.tsx
+++ src/components/ourServices/pdf_download_btn.tsx
@@ -15,9 +15,9 @@ const PdfDownloadButton: React.FC = () => {
       <a href={pdfppt} target='_blank'>
         <button
           onClick={handleDownloadClick}
-          className="py-2 px-4 text-lg font-semibold hover:underline duration-300 ease-in-out hover:text-teal-500 border-2 bg-gradient-to-b from-white from-45% to-zinc-400 rounded-3xl text-slate-800 hover:text-accent-foreground hover:scale-105 transition-all border-white shadow-md shadow-slate-600"
+          className="py-2 px-8 text-lg font-semibold hover:underline duration-300 ease-in-out hover:text-teal-500 border-2 bg-gradient-to-b from-white from-45% to-zinc-400 rounded-3xl text-slate-800 hover:text-accent-foreground hover:scale-105 transition-all border-white shadow-md shadow-slate-600"
         >
-          Download Brochure&rarr;
+          Download Brochure
         </button>
       </a>
     </div>
